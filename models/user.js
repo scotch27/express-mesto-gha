de5459154865f886c11eabcbada48911bfe21483
scotch <scotch27@mail.ru>
2023-07-23 22:57:20 +0300

    В userSchema добавлены поля по умолчанию

--- models/user.js
+++ models/user.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
-const { URL_REGEXP } = require('../utils/constants');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { URL_REGEXP } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Минимальная длина поля 2 символа'],
+    maxlength: [30, 'Максимальная длина поля 30 символов'],
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    default: 'Исследователь',
+    minlength: [2, 'Минимальная длина поля 2 символа'],
+    maxlength: [30, 'Максимальная длина поля 30 символов'],
   },
   avatar: {
     type: String,
     required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(v) {
         const regex = URL_REGEXP;
@@ -42,6 +46,27 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-});
+},
+{
+  versionKey: false,
+},
+);
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
