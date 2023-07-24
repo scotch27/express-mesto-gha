698959ba9f5ee7139abd48be0ada720b4534447a
scotch <scotch27@mail.ru>
2023-07-24 22:11:09 +0300

    Защитита API авторизацией

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/constants');
-const NotFoundError = require('../utils/errors/notFoundError');
+const UnauthorizedError = require('../utils/errors/unauthorizedError');
 
 module.exports = (req, res, next) => {
   let payload;
   try {
     const token = req.headers.authorization.replace('Bearer ', '');
     if (!token) {
-      return next(new NotFoundError('Необходима авторизация'));
+      return next(new UnauthorizedError('Необходима авторизация'));
     }
     payload = jwt.verify(token, JWT_SECRET);
   } catch (e) {
-    return next(new NotFoundError('Необходима авторизация'));
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
   next();
