505dd27b5121ff510bfe2293bc5d5a4875f1a6fb
scotch <scotch27@mail.ru>
2023-07-25 21:38:15 +0300

    Актуализирован auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,15 @@ const { JWT_SECRET } = require('../utils/constants');
 const UnauthorizedError = require('../utils/errors/unauthorizedError');
 
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    throw new UnauthorizedError('Необходима авторизация!');
+  }
   let payload;
   try {
-    const token = req.headers.authorization.replace('Bearer ', '');
-    if (!token) {
-      return next(new UnauthorizedError('Необходима авторизация'));
-    }
+    const token = authorization.replace('Bearer ', '');
     payload = jwt.verify(token, JWT_SECRET);
-  } catch (e) {
+  } catch (error) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
