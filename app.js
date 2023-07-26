6a4d2b826f0f06c8ec7e624d32ad0206c468563a
scotch <scotch27@mail.ru>
2023-07-26 13:56:01 +0300

    Обновления URL_REGEXP + rateLimit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit')
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const users = require('./routes/users');
@@ -12,11 +13,19 @@ const { validateLogin, validateRegister } = require('./utils/validators/userVali
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(helmet());
+app.use(limiter);
 
 // подключаемся к серверу mongo
 mongoose.connect(DB_URL);
