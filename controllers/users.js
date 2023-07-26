9ab67aedfc1332fcf331616386e978a625382361
scotch <scotch27@mail.ru>
2023-07-26 09:29:36 +0300

    Исправлен формат передачи токена

--- controllers/users.js
+++ controllers/users.js
@@ -118,6 +118,6 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      return res.send(token);
+      return res.send({ token });
     }).catch((error) => next(error));
 };
